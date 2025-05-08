adb72a53e864387b967ac7922e31441062390cfd
G384312023 <238431@st.takushoku-u.ac.jp>
2025-05-08 17:40:13 +0900

    25_05_08

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -1,18 +1,33 @@
-//////////////// ここは書き換えてはいけない！ 
+//////////////// ここは書き換えてはいけない！
 
 let campus = {
-	address: "八王子市館町",
-	buildingD: ["D101", "D102", "D103", "D201", "D202", "D203", "D204", "D205"],
-	lon: 35.624869704425,
-	lat: 139.28201056633
+  address: "八王子市館町",
+  buildingD: ["D101", "D102", "D103", "D201", "D202", "D203", "D204", "D205"],
+  lon: 35.624869704425,
+  lat: 139.28201056633,
 };
 
 let gakka = [
-	{name: "機械システム工学科", ename: "Department of Mechanical Systems Engineering"},
-	{name: "電子システム工学科", ename: "Department of Electronics and Computer Systems"},
-	{name: "情報工学科", ename: "Department of Computer Science"},
-	{name: "デザイン学科", ename: "Department of Design"}
+  {
+    name: "機械システム工学科",
+    ename: "Department of Mechanical Systems Engineering",
+  },
+  {
+    name: "電子システム工学科",
+    ename: "Department of Electronics and Computer Systems",
+  },
+  { name: "情報工学科", ename: "Department of Computer Science" },
+  { name: "デザイン学科", ename: "Department of Design" },
 ];
 
 //////////////// ここから下にプログラムを書きたそう!
 
+console.log(campus.address);
+
+for (room of campus.buildingD) {
+  console.log(room);
+}
+
+for (dep of gakka) {
+  console.log(dep.name);
+}
